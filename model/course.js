5bc7b5043a166456c5300a9e7542e17e0b09b4ae
thitiwat <thitiwat.ku@ku.th>
2023-02-04 23:53:05 +0700

    Add fill subject method

--- model/course.js
+++ model/course.js
@@ -1,3 +1,5 @@
+var Expression = require('./expression');
+
 class Course {
     constructor(nameDepartment, startYear, category = []) {
         this.nameDepartment = nameDepartment;
@@ -10,7 +12,67 @@ class Course {
     }
 
     fillSubject(subjectList){
-        
+        this.fillRoundOne(subjectList, this.category);
+        this.fillRoundTwo(subjectList, this.category);
+    }
+
+    //fill subject with condition sumAllCredit < atLeastCredit
+    fillRoundOne(subjectList, listCategory){
+        var countCredit = {}
+        for (var category of listCategory) {
+            for (let i=0; i<subjectList.length; i++){
+                if (category.expression == null)
+                    break;
+
+                if (!subjectList[i])
+                    continue;
+
+                let subject = subjectList[i];
+                let categoryName = category.categoryName;
+                if (!countCredit.hasOwnProperty(categoryName))
+                    countCredit[categoryName] = 0;
+
+                if (countCredit[categoryName] >= category.atLeastCredit)
+                    continue;
+                
+                var result = Expression.validateAll(subject.subject_code, category.Expression);
+                if (result){
+                    category.subject.push(subject);
+                    countCredit[categoryName] += subject.credit;
+                    
+                    //remove subject from list
+                    subjectList[i] = null;
+                }
+            }
+            
+            if (category.subCategory > 0)
+                this.fillRoundOne(subjectList, category.subCategory)
+        }
+    }
+
+    fillRoundTwo(subjectList, listCategory){
+        for (var category of listCategory) {
+            for (let i=0; i<subjectList.length; i++){
+                if (category.expression == null)
+                    break;
+
+                if (!subjectList[i])
+                    continue;
+
+                let subject = subjectList[i];
+                
+                var result = Expression.validateAll(subject.subject_code, category.expression);
+                if (result){
+                    category.subject.push(subject);
+                    
+                    //remove subject from list
+                    subjectList[i] = null;
+                }
+            }
+            
+            if (category.subCategory > 0)
+                this.fillRoundTwo(subjectList, category.subCategory)
+        }
     }
 }
 
